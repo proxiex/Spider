04ca3d1485032240e00ad734f40b0964d7258180
proxiex <difrentecho@gmail.com>
2016-09-28 01:46:36 -0700

    Update spider.py

--- spider.py
+++ spider.py
@@ -40,7 +40,9 @@ class Spider:
             Spider.add_links_to_queue(Spider.gather_links(page_url))
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
-            Spider.update_files()
+            Spider.update_files()            
+         else:
+            print("Nothing to Crawl")            
 
     # Converts raw response data into readable information and checks for proper html formatting
     @staticmethod
